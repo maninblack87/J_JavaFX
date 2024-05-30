1ed7ca104f4b0bad9b3f6bb152fd017d9a92daab
maninblack87 <maninblack87@naver.com>
2024-05-30 15:44:10 +0900

    240530 2nd

--- jeon_javafx/src/main/java/jeon_javafx/SecondaryController.java
+++ jeon_javafx/src/main/java/jeon_javafx/SecondaryController.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class SecondaryController {
+
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
     }
+
 }
