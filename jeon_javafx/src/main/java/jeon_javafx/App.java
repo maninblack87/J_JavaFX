4f2bccd473c20a6645d5a3faa3934ee0b1d846df
maninblack87 <maninblack87@naver.com>
2024-05-30 14:23:28 +0900

    240530 1st

--- jeon_javafx/src/main/java/jeon_javafx/App.java
+++ jeon_javafx/src/main/java/jeon_javafx/App.java
@@ -18,6 +18,9 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
+        stage.setTitle("전석환의 자바FX");
         stage.setScene(scene);
         stage.show();
     }
--- jeon_javafx/src/main/java/jeon_javafx/PrimaryController.java
+++ jeon_javafx/src/main/java/jeon_javafx/PrimaryController.java
@@ -2,8 +2,12 @@ package jeon_javafx;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class PrimaryController {
+
+    @FXML
+    private Label testLabel;
 
     @FXML
     private void switchToSecondary() throws IOException {
