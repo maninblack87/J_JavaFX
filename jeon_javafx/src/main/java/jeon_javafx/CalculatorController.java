aa63a6f9235789160fc4f2581786195917b6ee6b
maninblack87 <maninblack87@naver.com>
2024-06-14 16:03:07 +0900

    240614 2nd

---
+++ jeon_javafx/src/main/java/jeon_javafx/CalculatorController.java
@@ -0,0 +1,45 @@
+package jeon_javafx;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+public class CalculatorController {
+
+    // calculator.fxml에서 fx:id를 가져온다
+    @FXML
+    private TextField iptNum1;
+    @FXML
+    private TextField iptNum2;
+    @FXML
+    private Label operation;
+    @FXML
+    private Label result;
+
+    // 처음으로 돌아간다
+    @FXML
+    private void goPrimary() throws IOException{
+        App.setRoot("primary");
+    }
+
+    // 연산 타입을 변환한다
+    @FXML
+    private void operSum() throws IOException{
+        result.setText(Integer.toString(Integer.parseInt(iptNum1.getText())+Integer.parseInt(iptNum2.getText())));
+    }
+    @FXML
+    private void operSub() throws IOException{
+        result.setText(Integer.toString(Integer.parseInt(iptNum1.getText())-Integer.parseInt(iptNum2.getText())));
+    }
+    @FXML
+    private void operMul() throws IOException{
+        result.setText(Integer.toString(Integer.parseInt(iptNum1.getText())*Integer.parseInt(iptNum2.getText())));
+    }
+    @FXML
+    private void operDiv() throws IOException{
+        result.setText(Integer.toString(Integer.parseInt(iptNum1.getText())/Integer.parseInt(iptNum2.getText())));
+    }
+    
+}
